31838fc77dccfdc58c77a9e5d7c9298d295d8c50
Rafael Assis <rafazassis@gmail.com>
2015-04-10 20:32:24 -0300

    Initial Commit

---
+++ HappyPlaces/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyPlaces
+//
+//  Created by Rafael Assis on 4/10/15.
+//  Copyright (c) 2015 rafaiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HappyPlaces/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HappyPlaces
+//
+//  Created by Rafael Assis on 4/10/15.
+//  Copyright (c) 2015 rafaiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
